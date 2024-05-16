1b9d2a9785f5691a0af0cf010d263bdbd488b32a
Johanna135 <johanna.mascher@student.uibk.ac.at>
2024-05-16 16:11:58 +0200

    Datum formatiert und eingefügt

--- main.js
+++ main.js
@@ -39,20 +39,22 @@ async function showForecast(url) {
     console.log(jsondata);
     L.geoJSON(jsondata, {
         pointToLayer: function (feature, latlng) { // ich mach den Content um das wo zuerst hello world steht das dann drin steht
-            let content = `
+            let details = feature.properties.timeseries[0].data.instant.details; // wenn eckige Klammer im Original feature muss ich mit Index zugreifen ich mache eine Variable um auf die features zuzugreifen weil es so tief hinein geht und man das nicht immer schreiben will
+            let time = new Date(feature.properties.timeseries[0].time); // das new Date mach ein echtes Datum aus einem Datumsstring --> mit echtem Datum kann ich rechnen, nur mir string nicht
+            let content = ` 
+            <h4>Wettervorhersage für ${time.toLocaleString()}</h4>
             <ul>
-                <li>Luftdruck Meereshöhe(hPa): air_pressure_at_sea_level</li>
-                <li>Temperatur (°C): air_temperature</li>
-                <li>Bewölkungsgrad (%): cloud_area_fraction</li>
-                <li>Niederschlag (mm): precipitation_amount</li>
-                <li>Relative Luftfeuchte (%): relative_humidity</li>
-                <li>Windrichtung (°): wind_from_direction</li>
-                <li>Windgeschwindigkeit (km/h): wind_speed</li>
+                <li>Luftdruck Meereshöhe (hPa): ${details.air_pressure_at_sea_level} </li>
+                <li>Temperatur (°C): ${details.air_temperature}</li>
+                <li>Bewölkungsgrad (%): ${details.cloud_area_fraction}</li>
+                <li>Relative Luftfeuchte (%):  ${details.relative_humidity}</li>
+                <li>Windrichtung (°): ${details.wind_from_direction}</li>
+                <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed * 3.6)}</li>
             </ul>
-            `
-            L.popup(latlng, { content: '<p>Hello world!<br />This is a nice popup.</p>' })
-                .openOn(themaLayer.forecast);
-
+            `;
+            L.popup(latlng, {
+                content: content // das popup hat den content der variable content
+            }).openOn(themaLayer.forecast);
         }
     }).addTo(themaLayer.forecast);
 }
